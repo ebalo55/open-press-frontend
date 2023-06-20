4a47157757e4d8c203781c0438c7ae742410d1e7
ebalo55 <ebalo55@users.noreply.github.com>
2023-06-20 05:54:11 +0200

    feat: configuration interface addition

--- libs/config/src/lib/config.ts
+++ libs/config/src/lib/config.ts
@@ -1,4 +1,6 @@
-export const CONFIG = {
+import {ConfigInterface} from "@open-press/frontend-interfaces";
+
+export const CONFIG: ConfigInterface = {
     app_name: process.env.NEXT_PUBLIC_APP_NAME,
     backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
 };
---
+++ libs/frontend-interfaces/src/lib/extensibility/config-interface.ts
@@ -0,0 +1,4 @@
+export interface ConfigInterface {
+    app_name?: string;
+    backend_url?: string;
+}
