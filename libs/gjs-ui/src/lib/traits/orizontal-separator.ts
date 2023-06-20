e5989292d404513e5d279bbc9d2a818784970c85
ebalo <ebalo55@users.noreply.github.com>
2023-06-20 10:09:54 +0200

    fix: horizontal separator class explosion

--- libs/gjs-ui/src/lib/traits/orizontal-separator.ts
+++ libs/gjs-ui/src/lib/traits/orizontal-separator.ts
@@ -1,5 +1,4 @@
 import { TraitHorizontalSeparatorExtraSettings } from "@open-press/frontend-interfaces";
-import classNames from "classnames";
 import { ITraitView } from "grapesjs";
 
 export const TraitHorizontalSeparator: ITraitView = {
@@ -14,7 +13,7 @@ export const TraitHorizontalSeparator: ITraitView = {
 		element.style.border = "1px solid #9ca3af";
 
 		if (additional_classes) {
-			element.classList.add(...classNames(additional_classes));
+			element.classList.add(...additional_classes.split(" "));
 		}
 
 		return element;
