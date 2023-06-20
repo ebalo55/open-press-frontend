db9a5f25e680b741dcec5ec509191054fabaa669
ebalo55 <ebalo55@users.noreply.github.com>
2023-06-20 05:55:14 +0200

    fix: full page loader now forced to be full page

--- libs/components/src/lib/client/loader/full-page.tsx
+++ libs/components/src/lib/client/loader/full-page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { Center, Loader, Stack } from "@mantine/core";
-import { FC } from "react";
+import {Center, Loader, Stack} from "@mantine/core";
+import {FC} from "react";
 
 export const FullPage: FC = () => {
 	return (
-		<Center>
-			<Stack
-				align={"center"}
-				justify={"center"}
-			>
-				<Loader variant={"bars"} />
-			</Stack>
-		</Center>
-	);
+        <Center sx={{
+            height: "100dvh",
+            width: "100dvw",
+        }}>
+            <Stack
+                align={"center"}
+                justify={"center"}
+            >
+                <Loader variant={"bars"}/>
+            </Stack>
+        </Center>
+    );
 };
