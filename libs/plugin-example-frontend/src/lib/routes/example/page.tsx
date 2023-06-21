942a81a78a434b438a321f2e976d8fde4e94db16
ebalo <ebalo55@users.noreply.github.com>
2023-06-21 05:09:58 +0200

    feat: example plugin creation

---
+++ libs/plugin-example-frontend/src/lib/routes/example/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line */
+export interface PluginExampleFrontendProps {}
+
+export default function Page(props: PluginExampleFrontendProps) {
+	return (
+		<div>
+			<h1>Welcome to PluginExampleFrontend!</h1>
+		</div>
+	);
+}
